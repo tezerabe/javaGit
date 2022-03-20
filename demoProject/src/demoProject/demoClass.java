0f3ae379bb742e8500eb6c9d8e520384023e89e0
”tezerabe” <tezrerabe@gmail.com>
2022-03-19 19:15:56 -0700

    commited
    Signed-off-by: ”tezerabe” <”tezrerabe@gmail.com”>

---
+++ demoProject/src/demoProject/demoClass.java
@@ -0,0 +1,11 @@
+package demoProject;
+
+public class demoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("demo project");
+
+	}
+
+}
